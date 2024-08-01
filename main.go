c685b9d89009f506bccc67eb8c347eab05aef223
andrawicaksono <andrawicaksono14@gmail.com>
2024-08-02 00:33:24 +0700

    feat: add register user endpoint

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"startup-crowdfunding-backend/handler"
+	"startup-crowdfunding-backend/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	dsn := "bwa:bwa123@tcp(127.0.0.1:3306)/startup_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-  _, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is good")
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run(":4000")
 }
