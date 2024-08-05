936ce1f747559505fa0d6bcf9ab2a52444d9c474
andrawicaksono <andrawicaksono14@gmail.com>
2024-08-05 16:44:17 +0700

    feat: jwt auth, validate token

--- auth/service.go
+++ auth/service.go
@@ -1,9 +1,15 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type Service interface {
 	GenerateToken(id int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type service struct {
@@ -14,10 +20,20 @@ func NewService() *service {
 }
 
 var SECRET_KEY = []byte("BWASTARTUP_s3C23t_K3y")
+var LOGIN_EXPIRATION_DURATION = time.Duration(15) * time.Minute
 
 func (s *service) GenerateToken(userID int) (string, error) {
-	claims := jwt.MapClaims{}
-	claims["user_id"] = userID
+	type MyClaims struct {
+		jwt.RegisteredClaims
+		UserID int `json:"user_id"`
+	}
+
+	claims := MyClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(LOGIN_EXPIRATION_DURATION)),
+		},
+		UserID: userID,
+	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
@@ -27,4 +43,23 @@ func (s *service) GenerateToken(userID int) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (s *service) ValidateToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		method, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("invalid token")
+		} else if method != jwt.SigningMethodHS256 {
+			return nil, errors.New("invalid token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
